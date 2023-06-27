90c9f79f1bd140f86b011759ddf4dca203bca9a2
SKalltheway <samkaczynski@gmail.com>
2023-06-27 15:03:53 -0400

    Update backend_creating_grid.py
    new code

--- backend_creating_grid.py
+++ backend_creating_grid.py
@@ -1,28 +1,26 @@
 import random
+import random
 
-def generate_nfl_grid():
-    grid = []
-    teams = ["Team A", "Team B", "Team C", "Team D", "Team E", "Team F", "Team G", "Team H", "Team I"]  # Replace with the actual team names
+def generate_team_grid():
+    teams = ["Team A", "Team B", "Team C"]  # Replace with the actual team names
+    random.shuffle(teams)  # Randomize the team order
     
-    # Shuffle the team list
-    random.shuffle(teams)
+    grid = []
+    players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9"]  # Replace with actual player names
     
     # Generate the grid
     for i in range(3):
         row = []
         for j in range(3):
-            # Assign a random player to each cell
-            player = get_random_player()
-            row.append(f"{teams[i*3+j]}: {player}")
+            team = teams[i]
+            player = random.choice(players)
+            row.append(f"{team}: {player}")
+            players.remove(player)  # Remove the selected player from the list
         grid.append(row)
     
     # Print the grid
     for row in grid:
         print(" | ".join(row))
 
-def get_random_player():
-    players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9"]  # Replace with actual player names
-    return random.choice(players)
-
-# Generate and display the NFL grid with player names
-generate_nfl_grid()
+# Generate and display the team grid with players
+generate_team_grid()
