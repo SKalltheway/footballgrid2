f8255cd4ea7afc6c13a0f5dfb2aec3ea3b5b497f
SKalltheway <samkaczynski@gmail.com>
2023-06-29 15:18:14 -0400

    create dictionary which stores which players are right at which coordinates

--- backend_creating_grid.py
+++ backend_creating_grid.py
@@ -10,6 +10,7 @@ def generate_player_grid():
 
     team_map = dict({'Ari' : 'CARDINALS', 'Atl' : 'FALCONS', 'Bal' : 'RAVENS', 'Buf': 'BILLS', 'Car': 'PANTHERS', 'Cin': 'BENGALS', 'Cle' : 'BROWNS', 'Chi': 'BEARS', 'Dal': 'COWBOYS', 'Den':'BRONCOS', 'Det': 'LIONS', 'GB': 'PACKERS', 'Hou': 'TEXANS', 'Ind': 'COLTS', 'KC': 'CHIEFS', 'LAC': 'CHARGERS', 'SD': 'CHARGERS', 'LAR':'RAMS', 'Stl':'RAMS', 'LA': 'RAMS', 'Jax': 'JAGUARS', 'Mia':'DOLPHINS', 'Min': 'VIKINGS', 'NE': 'PATRIOTS', 'NO': 'SAINTS', 'NYG': 'GIANTS', 'NYJ': 'JETS', 'LV': 'RAIDERS', 'Oak': 'RAIDERS', 'Phi': 'EAGLES', 'SF': '49ERS', 'Sea': 'SEAHWAKS', 'Pit': 'STEELERS', 'TB': 'BUCCANEERS', 'Ten':'TITANS', 'Was': 'COMMANDERS'  })
     choice_dict = dict({'1': [0,0], '2': [1,0], '3': [2,0], '4': [0,1], '5': [1,1], '6': [2,1], '7': [0,2], '8': [1,2], '9': [2,2]})
+    correct_players = dict({'1': 'X', '2': 'X', '3': 'X', '4': 'X', '5':'X', '6': 'X', '7': 'X', '8': 'X', '9': 'X'})
     teams_x = [random.choice(teams_x_data)]
 
     teams_x_data.remove(teams_x[0])
@@ -35,7 +36,8 @@ def generate_player_grid():
 
     grid = []
     guesses_remaining = 9
-
+    correct_grids = []
+    num_correct = 0
     while guesses_remaining > 0:
         print("Press 1 for ", teams_x[0], " and ", teams_y[0])
         print("Press 2 for ", teams_x[1], " and ", teams_y[0])
@@ -47,10 +49,31 @@ def generate_player_grid():
         print("Press 8 for ", teams_x[1], " and ", teams_y[2])
         print("Press 9 for ", teams_x[2], " and ", teams_y[2])
         user_choice = input()
-        var = choice_dict.get(user_choice)
-        print(var)
         player = input(f"Enter player for {teams_x[choice_dict.get(user_choice)[0]]} and {teams_y[choice_dict.get(user_choice)[1]]}: ")
-
+        for index, row in df.iterrows():   
+            if player == row[0]:
+                player_teams_with_years = row[3].split()
+                player_teams = [item for item in player_teams_with_years if not (item.isdigit() or len(item) > 4 )]
+                y = 0
+                while y<len(player_teams):
+                    player_teams[y] = player_teams[y].translate({ord(","): None})
+                    y = y + 1
+                x = 0
+                team_names = []
+                while x<len(player_teams):
+                    team_names.insert(x, team_map.get(player_teams[x]))
+                    x=x+1
+        if all(var in team_names for var in [teams_x[choice_dict.get(user_choice)[0]],teams_y[choice_dict.get(user_choice)[1]] ]):
+            print('Correct!')
+            temp_player_dict = dict({user_choice: player})
+            correct_players.update(temp_player_dict)
+            correct_grids.insert(num_correct, user_choice)
+            num_correct = num_correct + 1
+        else:
+            print('Incorrect :(')
+        guesses_remaining = guesses_remaining - 1
+        print(correct_players)
+        
 
     # Generate the grid
     """""
