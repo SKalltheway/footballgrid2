3d0bea27babffca3ec8ac6fe2f7c797c861b5bbd
SKalltheway <samkaczynski@gmail.com>
2023-06-29 12:26:37 -0400

    logic working, goes square by square

--- backend_creating_grid.py
+++ backend_creating_grid.py
@@ -5,10 +5,10 @@ import pandas as pd
 DATA_PATH = 'all_NFL_players_ever.csv'
 
 def generate_player_grid():
-    teams_x_data = ["CARDINALS", "FALCONS", "RAVENS", "BILLS", "PANTHERS","BENGALS", "BROWNS", "BEARS", "COWBOYS", "BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS", "CHIEFS", "CHARGERS", "RAMS", "JAGUARS", "DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS", "GIANTS", "JETS", "RAIDERS", "EAGLES", "49ERS", "SEAHAWKS", "STEELERS", "BUCCANEERS", "TITANS", "REDSKINS"]
-    teams_y_data = ["CARDINALS", "FALCONS", "RAVENS", "BILLS", "PANTHERS","BENGALS", "BROWNS", "BEARS", "COWBOYS", "BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS", "CHIEFS", "CHARGERS", "RAMS", "JAGUARS", "DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS", "GIANTS", "JETS", "RAIDERS", "EAGLES", "49ERS", "SEAHAWKS", "STEELERS", "BUCCANEERS", "TITANS", "REDSKINS"]
+    teams_x_data = ["CARDINALS", "FALCONS", "RAVENS", "BILLS", "PANTHERS","BENGALS", "BROWNS", "BEARS", "COWBOYS", "BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS", "CHIEFS", "CHARGERS", "RAMS", "JAGUARS", "DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS", "GIANTS", "JETS", "RAIDERS", "EAGLES", "49ERS", "SEAHAWKS", "STEELERS", "BUCCANEERS", "TITANS", "COMMANDERS"]
+    teams_y_data = ["CARDINALS", "FALCONS", "RAVENS", "BILLS", "PANTHERS","BENGALS", "BROWNS", "BEARS", "COWBOYS", "BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS", "CHIEFS", "CHARGERS", "RAMS", "JAGUARS", "DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS", "GIANTS", "JETS", "RAIDERS", "EAGLES", "49ERS", "SEAHAWKS", "STEELERS", "BUCCANEERS", "TITANS", "COMMANDERS"]
 
-    team_map = dict({'Ari' : 'CARDINALS', 'Atl' : 'FALCONS',})
+    team_map = dict({'Ari' : 'CARDINALS', 'Atl' : 'FALCONS', 'Bal' : 'RAVENS', 'Buf': 'BILLS', 'Car': 'PANTHERS', 'Cin': 'BENGALS', 'Cle' : 'BROWNS', 'Chi': 'BEARS', 'Dal': 'COWBOYS', 'Den':'BRONCOS', 'Det': 'LIONS', 'GB': 'PACKERS', 'Hou': 'TEXANS', 'Ind': 'COLTS', 'KC': 'CHIEFS', 'LAC': 'CHARGERS', 'SD': 'CHARGERS', 'LAR':'RAMS', 'Stl':'RAMS', 'LA': 'RAMS', 'Jax': 'JAGUARS', 'Mia':'DOLPHINS', 'Min': 'VIKINGS', 'NE': 'PATRIOTS', 'NO': 'SAINTS', 'NYG': 'GIANTS', 'NYJ': 'JETS', 'LV': 'RAIDERS', 'Oak': 'RAIDERS', 'Phi': 'EAGLES', 'SF': '49ERS', 'Sea': 'SEAHWAKS', 'Pit': 'STEELERS', 'TB': 'BUCCANEERS', 'Ten':'TITANS', 'Was': 'COMMANDERS'  })
 
     teams_x = [random.choice(teams_x_data)]
 
@@ -34,27 +34,35 @@ def generate_player_grid():
     df = pd.read_csv(DATA_PATH)
 
     grid = []
+    guesses_remaining = 9
     
     # Generate the grid
     for i in range(3):
         row1 = []
         for j in range(3):
-            
+            print("Guesses Remaining:",  guesses_remaining)
             player = input(f"Enter player for {teams_x[i]} and {teams_y[j]}: ")
             for index, row in df.iterrows():   
                 if player == row[0]:
                     player_teams_with_years = row[3].split()
                     player_teams = [item for item in player_teams_with_years if not (item.isdigit() or len(item) > 4 )]
-                    j = 0
-                    while j<len(player_teams):
-                        player_teams[j] = player_teams[j].translate({ord(","): None})
-                        j = j + 1
-                    print(player_teams)
-                    i = 0
-                    while i<len(player_teams):
-                        print(team_map.get(player_teams[i]))
-                        i=i+1
-            row1.append(player)
+                    y = 0
+                    while y<len(player_teams):
+                        player_teams[y] = player_teams[y].translate({ord(","): None})
+                        y = y + 1
+                    x = 0
+                    team_names = []
+                    while x<len(player_teams):
+                        team_names.insert(x, team_map.get(player_teams[x]))
+                        x=x+1
+            if all(var in team_names for var in [teams_x[i], teams_y[j]]):
+                print('Correct!')
+                row1.append(player)
+            else:
+                print('Incorrect :(')
+                player = 'X'
+                row1.append(player)
+            guesses_remaining = guesses_remaining - 1
         grid.append(row1)
     
     # Print the grid
