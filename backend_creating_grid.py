79c31fa092726311e2a78857d84ff91249967773
SKalltheway <samkaczynski@gmail.com>
2023-06-29 14:02:23 -0400

    making new way to interact with the players and grid

--- backend_creating_grid.py
+++ backend_creating_grid.py
@@ -9,7 +9,7 @@ def generate_player_grid():
     teams_y_data = ["CARDINALS", "FALCONS", "RAVENS", "BILLS", "PANTHERS","BENGALS", "BROWNS", "BEARS", "COWBOYS", "BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS", "CHIEFS", "CHARGERS", "RAMS", "JAGUARS", "DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS", "GIANTS", "JETS", "RAIDERS", "EAGLES", "49ERS", "SEAHAWKS", "STEELERS", "BUCCANEERS", "TITANS", "COMMANDERS"]
 
     team_map = dict({'Ari' : 'CARDINALS', 'Atl' : 'FALCONS', 'Bal' : 'RAVENS', 'Buf': 'BILLS', 'Car': 'PANTHERS', 'Cin': 'BENGALS', 'Cle' : 'BROWNS', 'Chi': 'BEARS', 'Dal': 'COWBOYS', 'Den':'BRONCOS', 'Det': 'LIONS', 'GB': 'PACKERS', 'Hou': 'TEXANS', 'Ind': 'COLTS', 'KC': 'CHIEFS', 'LAC': 'CHARGERS', 'SD': 'CHARGERS', 'LAR':'RAMS', 'Stl':'RAMS', 'LA': 'RAMS', 'Jax': 'JAGUARS', 'Mia':'DOLPHINS', 'Min': 'VIKINGS', 'NE': 'PATRIOTS', 'NO': 'SAINTS', 'NYG': 'GIANTS', 'NYJ': 'JETS', 'LV': 'RAIDERS', 'Oak': 'RAIDERS', 'Phi': 'EAGLES', 'SF': '49ERS', 'Sea': 'SEAHWAKS', 'Pit': 'STEELERS', 'TB': 'BUCCANEERS', 'Ten':'TITANS', 'Was': 'COMMANDERS'  })
-
+    choice_dict = dict({'1': [0,0], '2': [1,0], '3': [2,0], '4': [0,1], '5': [1,1], '6': [2,1], '7': [0,2], '8': [1,2], '9': [2,2]})
     teams_x = [random.choice(teams_x_data)]
 
     teams_x_data.remove(teams_x[0])
@@ -35,8 +35,25 @@ def generate_player_grid():
 
     grid = []
     guesses_remaining = 9
-    
+
+    while guesses_remaining > 0:
+        print("Press 1 for ", teams_x[0], " and ", teams_y[0])
+        print("Press 2 for ", teams_x[1], " and ", teams_y[0])
+        print("Press 3 for ", teams_x[2], " and ", teams_y[0])
+        print("Press 4 for ", teams_x[0], " and ", teams_y[1])
+        print("Press 5 for ", teams_x[1], " and ", teams_y[1])
+        print("Press 6 for ", teams_x[2], " and ", teams_y[1])
+        print("Press 7 for ", teams_x[0], " and ", teams_y[2])
+        print("Press 8 for ", teams_x[1], " and ", teams_y[2])
+        print("Press 9 for ", teams_x[2], " and ", teams_y[2])
+        user_choice = input()
+        var = choice_dict.get(user_choice)
+        print(var)
+        player = input(f"Enter player for {teams_x[choice_dict.get(user_choice)[0]]} and {teams_y[choice_dict.get(user_choice)[1]]}: ")
+
+
     # Generate the grid
+    """""
     for i in range(3):
         row1 = []
         for j in range(3):
@@ -63,7 +80,7 @@ def generate_player_grid():
                 player = 'X'
                 row1.append(player)
             guesses_remaining = guesses_remaining - 1
-        grid.append(row1)
+    grid.append(row1)
     
     # Print the grid
     print("  |", end="")
@@ -78,6 +95,6 @@ def generate_player_grid():
             print(f" {player} |", end="")
         print()
         print("-" * (6 + 9 * len(teams_y)))
-
+"""
 # Generate and display the player grid
 generate_player_grid()
