9507f58534b0341a31eed9e72bda3408462a5cca
SKalltheway <samkaczynski@gmail.com>
2023-07-03 15:38:19 -0400

    changes

--- views.py
+++ views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import pandas as pd
+from csv import DictReader
 
 views = Blueprint(__name__, "views")
+with open("all_NFL_players_ever.csv") as f:
+    player_names = [row["Player"] for row in DictReader(f)]
 
-@views.route("/")
+
+
+@views.route("/", methods=["POST", "GET"])
 def home():
+    
     #return render_template('index.html', team = "Colts")
-    return render_template('grid.html')
+    return render_template('search.html', playernames = player_names)
